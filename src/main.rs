fbdeedff54f756361c57410ec0dc6f8ac0ad6eb0
Felipe Pires <felipeaugustodluz@hotmail.com>
2023-06-27 00:32:13 -0300

    Subindo arquivos inicias, com o RabbitMQ configurado

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use amqprs::{
+    connection::{Connection, OpenConnectionArguments},
+    callbacks::{DefaultConnectionCallback, DefaultChannelCallback}, channel::{QueueDeclareArguments, BasicPublishArguments}, BasicProperties
+};
+use tokio;
+use tokio::io::Error as TError;
+use std::{thread, time::{self, UNIX_EPOCH}};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<TError>> {
+    let conn = Connection::open(&OpenConnectionArguments::new(
+        "localhost",
+        5672,
+        "guest",
+        "guest",
+    ))
+    .await.unwrap();
+    conn.register_callback(DefaultConnectionCallback).await.unwrap();
+    
+    let ch = conn.open_channel(None).await.unwrap();
+    ch.register_callback(DefaultChannelCallback).await.unwrap();
+    
+    let q_args = QueueDeclareArguments::default()
+        .queue(String::from("hello"))
+        .durable(false)
+        .finish();
+    let (queue_name, _, _) = ch.queue_declare(q_args).await.unwrap().unwrap();
+    let mut counter : u64 = 0;
+
+    let max_loop: u64 = time::SystemTime::now()
+    .duration_since(UNIX_EPOCH)
+    .unwrap()
+    .as_secs() / 10055;
+    
+    loop {
+        let payload = String::from(" {id: ".to_owned() + &counter.to_string() + " message: 'Hello world!'} ").into_bytes();
+        let publish_args = BasicPublishArguments::new("", &queue_name);
+        let props = BasicProperties::default().with_delivery_mode(2).finish();
+
+        ch.basic_publish( props, payload, publish_args).await.unwrap();
+        eprintln!(" [{}] Sent \"Hello World!\"", counter);
+        counter += 1;
+        thread::sleep(time::Duration::new(1,0));
+
+        println!("{}", max_loop);
+
+        if counter > max_loop {
+            break;
+        }
+    }
+    
+    conn.close().await.unwrap();
+    
+    Ok(())
+}
